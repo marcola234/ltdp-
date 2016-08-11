f5edbceaf0a9b7e39508519463fdf48dde33c169
aluno <aluno@labwbs19>
2016-08-11 08:47:52 -0300

    conexao

--- projeto-feira/newfile.php
+++ projeto-feira/newfile.php
@@ -2,4 +2,23 @@
 	echo "GET:" . var_dump($_GET) . "<br>";
 	echo "POST:" . var_dump($_POST) . "<br>";
 	echo "posicao nome" . $_POST['nome'];
+
+	
+	/*configuçao de endereço do banco de dados*/
+ $servidor = "localhost";
+ $usuario = "root";
+ $senha = "" ;
+ 
+/*configuraçao de acesso do banco de dados */
+ $nome_banco = "bd_centro_interesse";
+ 
+ $conexao = mysql_connect($servidor, $usuario, $senha)
+ 
+ $banco = mysql_select_db($nome_banco, $conexao);
+ 
+ 
+ 
+	
+	
+	
 ?>
