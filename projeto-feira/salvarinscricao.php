5ff03623548f784e81aad0f0b08ba44c0fed8ec2
aluno <aluno@labwbs19>
2016-08-17 09:41:49 -0300

    configuraçoes necessarias por aula

--- projeto-feira/salvarinscricao.php
+++ projeto-feira/salvarinscricao.php
@@ -12,9 +12,7 @@
 /*configuraçao de acesso do banco de dados */
  $nome_banco = "bd_centro_interesse";
  
- $conexao = mysql_connect($servidor, $usuario, $senha)
- 
- $banco = mysql_select_db($nome_banco, $conexao);
+ $conexao = mysql_connect($servidor, $usuario, $senha);
  
  $banco = mysql_select_db($nome_banco, $conexao) or die( "nao foi possivel conectar ao banco MYSQL");
  var_dump ($banco);
@@ -23,7 +21,7 @@
  echo "<br>";
  /*verifica se a conexao realmente foi criada*/
  /*se (nao conexao) entao, ou seja, conexao e falsa*/
- if ($conexao) {
+ if (!$conexao) {
  	echo "nao foi possivel conectar ao banco MYSQL.";
  	exit;
  }/*senao*/
